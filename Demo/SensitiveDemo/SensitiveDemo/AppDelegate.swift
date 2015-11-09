226b8b84532fccb6ae60b43c6e98af9e84fd7add
Igor Matyushkin <igormatyushkin014@gmail.com>
2015-11-09 03:36:43 +0300

    Added demo project

---
+++ Demo/SensitiveDemo/SensitiveDemo/AppDelegate.swift
@@ -0,0 +1,89 @@
+//
+//  AppDelegate.swift
+//  SensitiveDemo
+//
+//  Created by Igor Matyushkin on 09.11.15.
+//  Copyright © 2015 Igor Matyushkin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: Class variables & properties
+    
+    
+    // MARK: Class methods
+    
+    
+    // MARK: Initializers
+    
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    
+    // MARK: Variables & properties
+    
+    var window: UIWindow?
+    
+    
+    // MARK: Public methods
+    
+    
+    // MARK: Private methods
+    
+    
+    // MARK: Protocol methods
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Create window
+        
+        let frameForWindow = UIScreen.mainScreen().bounds
+        window = UIWindow(frame: frameForWindow)
+        window!.backgroundColor = .whiteColor()
+        window!.makeKeyAndVisible()
+        
+        
+        // Switch to view controller
+        
+        let mainViewController = MainViewController(nibName: "MainViewController", bundle: nil)
+        
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        navigationController.navigationBarHidden = false
+        
+        window!.rootViewController = navigationController
+        
+        
+        // Return result
+        
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+}
+
---
+++ Demo/SensitiveDemo/SensitiveDemo/Flows/Main/MainViewController/MainViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MainViewController.swift
+//  SensitiveDemo
+//
+//  Created by Igor Matyushkin on 09.11.15.
+//  Copyright © 2015 Igor Matyushkin. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // MARK: Class variables & properties
+    
+    
+    // MARK: Class methods
+    
+    
+    // MARK: Initializers
+    
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    
+    // MARK: Outlets
+    
+    
+    // MARK: Variables & properties
+    
+    
+    // MARK: Public methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: Private methods
+    
+    
+    // MARK: Actions
+    
+    
+    // MARK: Protocol methods
+    
+}
