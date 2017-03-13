39beeb359c85f7f48d47102381980e6528b5cef5
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-03-13 22:38:05 +0300

    Added new types

---
+++ Sensitive/GestureRecognizers/Swipe/SwipeDirection.swift
@@ -0,0 +1,16 @@
+//
+//  SwipeDirection.swift
+//  SensitiveDemo
+//
+//  Created by Igor Matyushkin on 13.03.17.
+//  Copyright © 2017 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public enum SwipeDirection {
+    case up
+    case right
+    case down
+    case left
+}
---
+++ Sensitive/Types/GestureHandlerReuseCount.swift
@@ -0,0 +1,14 @@
+//
+//  GestureHandlerReuseCount.swift
+//  SensitiveDemo
+//
+//  Created by Igor Matyushkin on 13.03.17.
+//  Copyright © 2017 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public enum GestureHandlerReuseCount {
+    case once
+    case always
+}
