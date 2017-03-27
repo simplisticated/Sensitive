ef49ed33258f78b78bd7382483d03102d9e6bab4
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-03-27 15:37:35 +0300

    Added methods for managing TouchGestureRecognizer with UIView extension

--- Sensitive/Extensions/Array/ArrayExtensionViews.swift
+++ Sensitive/Extensions/Array/ArrayExtensionViews.swift
@@ -248,6 +248,39 @@ public extension Array where Element: UIView {
         return self
     }
     
+    /**
+    Adds `TouchGestureRecognizer` instance to each view in array.
+    
+    - parameters:
+        - handle: Called when `TouchGestureRecognizer` instance changes its state.
+        - configure: Allows to configure `TouchGestureRecognizer` instance.
+            This method is called for each gesture recognizer added to view in array.
+            It's recommended to change settings of gesture recognizer inside of this block.
+    
+    - returns:
+        Reference to receiver for support of chain calls.
+    */
+    @discardableResult
+    public func onTouch(handle: @escaping GestureRecognizerHandlerBlock<TouchGestureRecognizer>, configure: GestureRecognizerConfigurationBlock<TouchGestureRecognizer>?) -> Array {
+        /*
+         * Add gesture recognizer to each view in array.
+         */
+        
+        for view in self {
+            view.onTouch(when: .always, handle: { (gestureRecognizer) in
+                handle(gestureRecognizer)
+            }, configure: { (gestureRecognizer) in
+                configure?(gestureRecognizer)
+            })
+        }
+        
+        /*
+         * Return result.
+         */
+        
+        return self
+    }
+    
     // MARK: Private object methods
     
 }
--- Sensitive/Extensions/View/UIViewExtensionGestures.swift
+++ Sensitive/Extensions/View/UIViewExtensionGestures.swift
@@ -498,6 +498,65 @@ public extension UIView {
         return onScreenEdgePan(when: .always, handle: handle, configure: nil)
     }
     
+    /**
+    Adds `TouchGestureRecognizer` instance to view.
+    
+    - parameters:
+        - when: Represents how many times gesture should be handled.
+        - handle: Called when `TouchGestureRecognizer` instance changes its state.
+        - configure: Allows to configure `TouchRecognizer` instance.
+            It's recommended to change settings of gesture recognizer inside of this block.
+    
+    - returns:
+        Reference to receiver for support of chain calls.
+    */
+    @discardableResult
+    public func onTouch(when handlerReuseCount: GestureHandlerReuseCount, handle: @escaping GestureRecognizerHandlerBlock<TouchGestureRecognizer>, configure: GestureRecognizerConfigurationBlock<TouchGestureRecognizer>?) -> Self {
+        /*
+         * Obtain handler block for gesture recognizer.
+         */
+        
+        let handlerBlockForTouchRecognizer = stv_applyReuseCount(reuseCount: handlerReuseCount, toHandlerBlock: handle)
+        
+        /*
+         * Create gesture recognizer instance.
+         */
+        
+        let screenEdgePanGestureRecognizer = TouchGestureRecognizer(handlerBlock: handlerBlockForTouchRecognizer)
+        
+        /*
+         * Add gesture recognizer to receiver.
+         */
+        
+        addGestureRecognizer(screenEdgePanGestureRecognizer)
+        
+        /*
+         * Call configuration block for gesture recognizer.
+         */
+        
+        configure?(screenEdgePanGestureRecognizer)
+        
+        /*
+         * Return receiver's instance for support of chain calls.
+         */
+        
+        return self
+    }
+    
+    /**
+    Adds `TouchGestureRecognizer` instance to view.
+    
+    - parameters:
+        - handle: Called when `TouchGestureRecognizer` instance changes its state.
+    
+    - returns:
+        Reference to receiver for support of chain calls.
+    */
+    @discardableResult
+    public func onTouch(handle: @escaping GestureRecognizerHandlerBlock<TouchGestureRecognizer>) -> Self {
+        return onTouch(when: .always, handle: handle, configure: nil)
+    }
+    
     // MARK: Private object methods
     
     fileprivate func stv_numberOfTimesGestureRecognizerIsHandled(gestureRecognizer: UIGestureRecognizer) -> Int? {
